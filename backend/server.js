beacbdaf0c57af48b44b1a689f5c149b82064642
unknown <harshmetkel24@gmail.com>
2023-01-05 17:53:46 +0530

    trying cors

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,19 @@ const userRoutes = require("./routes/user");
 // express app
 const app = express();
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "DELETE, PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  if ("OPTIONS" == req.method) {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 // middleware
 app.use(express.json());
